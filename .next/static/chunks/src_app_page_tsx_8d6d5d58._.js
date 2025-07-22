24e978b1fb731fe608ed1b70dfd2f9aa422c19bc
chandusinner <65268662+Jonnessinner@users.noreply.github.com>
2025-07-22 11:29:30 +0530

    Add .next/static/chunks/src_app_page_tsx_8d6d5d58._.js 1753163969665

---
+++ .next/static/chunks/src_app_page_tsx_8d6d5d58._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d75cf64a._.js",
+  "static/chunks/src_2188f740._.js"
+],
+    source: "dynamic"
+});
